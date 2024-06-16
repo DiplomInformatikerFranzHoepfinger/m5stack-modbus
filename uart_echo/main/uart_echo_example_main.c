d2205bc1186bb86b67956257f36fd0bb44e9d79c
Franz Höpfinger <mail@franzhoepfinger.de>
2024-06-16 16:26:06 +0200

    Add string.h library for string functions.
    - Include <string.h> library for string functions.

--- uart_echo/main/uart_echo_example_main.c
+++ uart_echo/main/uart_echo_example_main.c
@@ -7,6 +7,7 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 #include <stdio.h>
+#include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/uart.h"
